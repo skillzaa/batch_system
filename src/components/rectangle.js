62524e2c3141c4a78f7a49dee4b986ccda2e94c4
skilzaa <skillzaa.com@gmail.com>
2022-01-08 12:53:18 +0500

    welcome to bilzaa2d

--- src/components/rectangle.js
+++ src/components/rectangle.js
@@ -1,10 +1,6 @@
 export default function rectangle(item){
     let compcon = new PIXI.Container();
-
-    let rectangle = new PIXI.Graphics();
-    rectangle.beginFill(item.color); //yellow 0xFFFF00
-    // rectangle.lineStyle(5, 0xFF0000); //red
-    rectangle.drawRect(item.x, item.y, item.width, item.height);
+//----------------------------------------
     const style = new PIXI.TextStyle({
         dropShadow: true,
         dropShadowAlpha: 0,
@@ -14,18 +10,30 @@ export default function rectangle(item){
         fill: "#d70f12",
         fontFamily: "Helvetica",
         fontSize: 20,
+        strokeThickness:4,
         fontStyle: "oblique",
         fontWeight: "bold",
         lineJoin: "round",
         miterLimit: 36,
-        stroke: "#0c0c0d",
+        // stroke: "#0c0c0d",
+        stroke: '#000000',
         strokeThickness: 1
     });
-    const text = new PIXI.Text('Welcome To Bilzaa2d', style);
+    let text_content = 'Welcome To Bilzaa2d';
+    const text = new PIXI.Text(text_content, style);
     text.resolution = 4;
-// wiz.app.stage.addChild(text);
-    rectangle.endFill();
 
+    let textMetrics = PIXI.TextMetrics.measureText(text_content, style);
+    // console.log(textMetrics);
+
+//-------------------------------------------    
+let rectangle = new PIXI.Graphics();
+    rectangle.beginFill(item.color); //yellow 0xFFFF00
+    // rectangle.lineStyle(5, 0xFF0000); //red
+
+    rectangle.drawRect(item.x, item.y, textMetrics.width, textMetrics.height);
+    rectangle.endFill();
+//------------------------------------------------
 
     compcon.addChild(rectangle);
     compcon.addChild(text);
